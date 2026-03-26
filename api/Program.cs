e40d3f713097b360875a6dd785b22f50a0d2c9b1
iyed ben aicha <i.benaicha@kiantec.co>
2026-03-26 17:46:11 +0100

    fix builder

--- api/Program.cs
+++ api/Program.cs
@@ -2,25 +2,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 // Only use HTTPS redirection in Production
 app.UseHttpsRedirection();
 
-// // Add a root endpoint
-// app.MapGet("/", () => "API is running. Visit /weatherforecast for weather data or /openapi for API docs")
-//     .WithName("Root")
-//     .WithOpenApi();
+// Add a root endpoint
+app.MapGet("/", () => "API is running. Visit /weatherforecast for weather data or /openapi for API docs")
+    .WithName("Root")
+    .WithOpenApi();
 
 app.Run();
